ff88dd948f33695cdd197c72cdf827b523904175
unko-chan <kevinliang0303@gmail.com>
2021-01-17 23:28:26 -0800

    refactored with methods and OOP

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false, //By default, all new tasks that are added in are incomplete.
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+    logState: function () {
+      console.log(
+        `${this.title} has${this.complete ? " " : " not "}been completed` //task true? otherwise, print not.
+      );
+    },
+
+    markCompleted: function () {
+      this.complete = true;
+    },
+
+  };
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+// // logs task state
+// function logTaskState(task) {
+//   console.log(
+//     `${task.title} has${task.complete ? " " : " not "}been completed` //task true? otherwise, print not.
+//   );
+// }
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+// // this function will change a task from false to true
+// function completeTask(task) {
+//   task.complete = true;
+// }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask(
+  "Clean Cat Litter",
+  "Take all the 💩 out of the litter box"
+);
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+//task1 is a function that does newTask(),
+//task1's parameters are plugged into newTask() parameters
+//so when we do logState, or markCompleted, those object functions
+//know will have the proper values from the parameters
+//that were passed in.
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+// console.log(tasks);
